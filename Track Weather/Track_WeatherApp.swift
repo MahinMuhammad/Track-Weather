9061bfb5e5ac6e04f1a8d04acd9386d73f5ffb5d
MahinMuhammad <amimahinurrahman@gmail.com>
2024-02-18 23:25:15 +0600

    .gitignore added

---
+++ Track Weather/Track_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Track_WeatherApp.swift
+//  Track Weather
+//
+//  Created by Md. Mahinur Rahman on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Track_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
